7407a1feae48e44b3a237aa9ce9ce46777f3c90f
shahriar <7091945+hsleonis@users.noreply.github.com>
2017-11-01 10:52:47 +0600

    Add bulk image resizer

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  @author: Hasan Shahriar
+ *  Resize Images proportionaly example
+ */
+
+require_once ('class.imageresizer.php');
+
+// Create thumbnails
+$args = array(
+    'height'    => 400,
+    'width'     => 270,
+    'is_crop_hard' => 1
+);
+$img = new ImageResizer($args);
+$img->create();
+
+// $img->createThumbnail('Desires_LB_MF16_7290.jpg',300,450);
